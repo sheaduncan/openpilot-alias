b93a3ad07276cc76168df3f691275df6ebf38034
rockerrock137 <rockerrock137@gmail.com>
2023-04-25 11:11:34 -0400

    Ford Torque

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -1,100 +1,57 @@
-from cereal import car
-from common.numpy_fast import clip
+from common.numpy_fast import interp, clip
+from selfdrive.car.ford.fordcan import spam_cancel_button, ParkAid_Data, EngVehicleSpThrottle2, BrakeSysFeatures
+from selfdrive.car.ford.values import CarControllerParams, TSS2_RADAR_INIT
 from opendbc.can.packer import CANPacker
-from selfdrive.car import apply_std_steer_angle_limits
-from selfdrive.car.ford.fordcan import create_acc_command, create_acc_ui_msg, create_button_msg, create_lat_ctl_msg, \
-  create_lat_ctl2_msg, create_lka_msg, create_lkas_ui_msg
-from selfdrive.car.ford.values import CANBUS, CANFD_CARS, CarControllerParams
+from common.conversions import Conversions as CV
+from selfdrive.car import make_can_msg
 
-VisualAlert = car.CarControl.HUDControl.VisualAlert
-
-
-class CarController:
+class CarController():
   def __init__(self, dbc_name, CP, VM):
-    self.CP = CP
-    self.VM = VM
     self.packer = CANPacker(dbc_name)
+    self.params = CarControllerParams
+    self.steer_delay = 1
+    self.steer_enabled = False
     self.frame = 0
 
-    self.apply_curvature_last = 0
-    self.main_on_last = False
-    self.lkas_enabled_last = False
-    self.steer_alert_last = False
-
   def update(self, CC, CS, now_nanos):
+    actuators = CC.actuators
     can_sends = []
 
-    actuators = CC.actuators
-    hud_control = CC.hudControl
+    #*** TSS2 Radar Init  ***
+    for (addr, bus, fr_step, vl) in TSS2_RADAR_INIT:
+      if self.frame % fr_step == 0:
+        can_sends.append(make_can_msg(addr, vl, bus))
+        #print(addr, vl, bus)
 
-    main_on = CS.out.cruiseState.available
-    steer_alert = hud_control.visualAlert in (VisualAlert.steerRequired, VisualAlert.ldw)
-
-    ### acc buttons ###
+    # Send cancel button if requested
     if CC.cruiseControl.cancel:
-      can_sends.append(create_button_msg(self.packer, CS.buttons_stock_values, cancel=True))
-      can_sends.append(create_button_msg(self.packer, CS.buttons_stock_values, cancel=True, bus=CANBUS.main))
-    elif CC.cruiseControl.resume and (self.frame % CarControllerParams.BUTTONS_STEP) == 0:
-      can_sends.append(create_button_msg(self.packer, CS.buttons_stock_values, resume=True))
-      can_sends.append(create_button_msg(self.packer, CS.buttons_stock_values, resume=True, bus=CANBUS.main))
-    # if stock lane centering isn't off, send a button press to toggle it off
-    # the stock system checks for steering pressed, and eventually disengages cruise control
-    elif CS.acc_tja_status_stock_values["Tja_D_Stat"] != 0 and (self.frame % CarControllerParams.ACC_UI_STEP) == 0:
-      can_sends.append(create_button_msg(self.packer, CS.buttons_stock_values, tja_toggle=True))
+      can_sends.append(spam_cancel_button(self.packer))
 
-    ### lateral control ###
-    # send steering commands at 20Hz
-    if (self.frame % CarControllerParams.STEER_STEP) == 0:
-      if CC.latActive:
-        # apply limits to curvature and clip to signal range
-        apply_curvature = apply_std_steer_angle_limits(actuators.curvature, self.apply_curvature_last, CS.out.vEgo, CarControllerParams)
-        apply_curvature = clip(apply_curvature, -CarControllerParams.CURVATURE_MAX, CarControllerParams.CURVATURE_MAX)
+    if True:
+      apply_speed = CS.out.vEgoRaw * CV.MS_TO_KPH
+      apply_steer = CS.out.steeringAngleDeg
+
+      if CC.enabled:
+        apply_speed = 0
+        if self.steer_delay <= 150:
+          self.steer_delay += 1
+        else:
+          self.steer_enabled = True
+          steer = interp(actuators.steer, [-1, 1], [-20, 20])
+          steer = clip(steer, -10, 10)
+          apply_steer = CS.out.steeringAngleDeg + steer
+          print(actuators.steer, CS.out.steeringAngleDeg - apply_steer)
       else:
-        apply_curvature = 0.
+        self.steer_delay = 1
+        self.steer_enabled = False
 
-      self.apply_curvature_last = apply_curvature
-      can_sends.append(create_lka_msg(self.packer))
-
-      if self.CP.carFingerprint in CANFD_CARS:
-        # TODO: extended mode
-        mode = 1 if CC.latActive else 0
-        counter = self.frame // CarControllerParams.STEER_STEP
-        can_sends.append(create_lat_ctl2_msg(self.packer, mode, 0., 0., -apply_curvature, 0., counter))
-      else:
-        can_sends.append(create_lat_ctl_msg(self.packer, CC.latActive, 0., 0., -apply_curvature, 0.))
-
-    ### longitudinal control ###
-    # send acc command at 50Hz
-    if self.CP.openpilotLongitudinalControl and (self.frame % CarControllerParams.ACC_CONTROL_STEP) == 0:
-      accel = clip(actuators.accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
-
-      precharge_brake = accel < -0.1
-      if accel > -0.5:
-        gas = accel
-        decel = False
-      else:
-        gas = -5.0
-        decel = True
-
-      can_sends.append(create_acc_command(self.packer, CC.longActive, gas, accel, precharge_brake, decel))
-
-    ### ui ###
-    send_ui = (self.main_on_last != main_on) or (self.lkas_enabled_last != CC.latActive) or (self.steer_alert_last != steer_alert)
-
-    # send lkas ui command at 1Hz or if ui state changes
-    if (self.frame % CarControllerParams.LKAS_UI_STEP) == 0 or send_ui:
-      can_sends.append(create_lkas_ui_msg(self.packer, main_on, CC.latActive, steer_alert, hud_control, CS.lkas_status_stock_values))
-
-    # send acc ui command at 20Hz or if ui state changes
-    if (self.frame % CarControllerParams.ACC_UI_STEP) == 0 or send_ui:
-      can_sends.append(create_acc_ui_msg(self.packer, main_on, CC.latActive, hud_control, CS.acc_tja_status_stock_values))
-
-    self.main_on_last = main_on
-    self.lkas_enabled_last = CC.latActive
-    self.steer_alert_last = steer_alert
+      can_sends.append(BrakeSysFeatures(self.packer, self.frame, apply_speed))
+      can_sends.append(EngVehicleSpThrottle2(self.packer, self.frame, apply_speed, CS.out.gearShifter))
+      can_sends.append(ParkAid_Data(self.packer, self.steer_enabled, apply_steer, CS.sappControlState, CS.out.standstill))
+      self.frame += 1
 
     new_actuators = actuators.copy()
-    new_actuators.curvature = self.apply_curvature_last
+    new_actuators.steeringAngleDeg = apply_steer
 
     self.frame += 1
     return new_actuators, can_sends
--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -1,219 +1,75 @@
+from common.numpy_fast import clip
+from common.conversions import Conversions as CV
 from cereal import car
-from selfdrive.car.ford.values import CANBUS
 
-HUDControl = car.CarControl.HUDControl
+GearShifter = car.CarState.GearShifter
 
+def fordchecksum(cnt, speed):
+  # Checksum is 255 - cnt - speed - df (data qualifier, 3 signals VALID data) with bitwise shifting and rounding on the speed.
+  speed = int(round(speed / 0.01, 2))
+  top = speed >> 8
+  bottom = speed & 0xff
+  cs = 255 - cnt - top - bottom - 3
+  if cs < 0:
+    cs = cs + 255
+  return cs
 
-def calculate_lat_ctl2_checksum(mode: int, counter: int, dat: bytearray):
-  checksum = mode + counter
-  checksum += dat[2] + ((dat[3] & 0xE0) >> 5)           # curvature
-  checksum += dat[6] + ((dat[7] & 0xE0) >> 5)           # curvature rate
-  checksum += (dat[3] & 0x1F) + ((dat[4] & 0xFC) >> 2)  # path angle
-  checksum += (dat[4] & 0x3) + dat[5]                   # path offset
-  return 0xFF - (checksum & 0xFF)
-
-
-def create_lka_msg(packer):
-  """
-  Creates an empty CAN message for the Ford LKA Command.
-
-  This command can apply "Lane Keeping Aid" manoeuvres, which are subject to the PSCM lockout.
-
-  Frequency is 20Hz.
-  """
-
-  return packer.make_can_msg("Lane_Assist_Data1", CANBUS.main, {})
-
-
-def create_lat_ctl_msg(packer, lat_active: bool, path_offset: float, path_angle: float, curvature: float,
-                       curvature_rate: float):
-  """
-  Creates a CAN message for the Ford TJA/LCA Command.
-
-  This command can apply "Lane Centering" manoeuvres: continuous lane centering for traffic jam assist and highway
-  driving. It is not subject to the PSCM lockout.
-
-  Ford lane centering command uses a third order polynomial to describe the road centerline. The polynomial is defined
-  by the following coefficients:
-    c0: lateral offset between the vehicle and the centerline (positive is right)
-    c1: heading angle between the vehicle and the centerline (positive is right)
-    c2: curvature of the centerline (positive is left)
-    c3: rate of change of curvature of the centerline
-  As the PSCM combines this information with other sensor data, such as the vehicle's yaw rate and speed, the steering
-  angle cannot be easily controlled.
-
-  The PSCM should be configured to accept TJA/LCA commands before these commands will be processed. This can be done
-  using tools such as Forscan.
-
-  Frequency is 20Hz.
-  """
-
+def spam_cancel_button(packer):
   values = {
-    "LatCtlRng_L_Max": 0,                       # Unknown [0|126] meter
-    "HandsOffCnfm_B_Rq": 0,                     # Unknown: 0=Inactive, 1=Active [0|1]
-    "LatCtl_D_Rq": 1 if lat_active else 0,      # Mode: 0=None, 1=ContinuousPathFollowing, 2=InterventionLeft,
-                                                #       3=InterventionRight, 4-7=NotUsed [0|7]
-    "LatCtlRampType_D_Rq": 0,                   # Ramp speed: 0=Slow, 1=Medium, 2=Fast, 3=Immediate [0|3]
-                                                #             Makes no difference with curvature control
-    "LatCtlPrecision_D_Rq": 1,                  # Precision: 0=Comfortable, 1=Precise, 2/3=NotUsed [0|3]
-                                                #            The stock system always uses comfortable
-    "LatCtlPathOffst_L_Actl": path_offset,      # Path offset [-5.12|5.11] meter
-    "LatCtlPath_An_Actl": path_angle,           # Path angle [-0.5|0.5235] radians
-    "LatCtlCurv_NoRate_Actl": curvature_rate,   # Curvature rate [-0.001024|0.00102375] 1/meter^2
-    "LatCtlCurv_No_Actl": curvature,            # Curvature [-0.02|0.02094] 1/meter
+    "Cancel": 1
   }
-  return packer.make_can_msg("LateralMotionControl", CANBUS.main, values)
+  return packer.make_can_msg("Steering_Buttons", 0, values)
 
-
-def create_lat_ctl2_msg(packer, mode: int, path_offset: float, path_angle: float, curvature: float,
-                        curvature_rate: float, counter: int):
-  """
-  Create a CAN message for the new Ford Lane Centering command.
-
-  This message is used on the CAN FD platform and replaces the old LateralMotionControl message. It is similar but has
-  additional signals for a counter and checksum.
-
-  Frequency is 20Hz.
-  """
-
-  values = {
-    "LatCtl_D2_Rq": mode,                       # Mode: 0=None, 1=PathFollowingLimitedMode, 2=PathFollowingExtendedMode,
-                                                #       3=SafeRampOut, 4-7=NotUsed [0|7]
-    "LatCtlRampType_D_Rq": 0,                   # 0=Slow, 1=Medium, 2=Fast, 3=Immediate [0|3]
-    "LatCtlPrecision_D_Rq": 1,                  # 0=Comfortable, 1=Precise, 2/3=NotUsed [0|3]
-    "LatCtlPathOffst_L_Actl": path_offset,      # [-5.12|5.11] meter
-    "LatCtlPath_An_Actl": path_angle,           # [-0.5|0.5235] radians
-    "LatCtlCurv_No_Actl": curvature,            # [-0.02|0.02094] 1/meter
-    "LatCtlCrv_NoRate2_Actl": curvature_rate,   # [-0.001024|0.001023] 1/meter^2
-    "HandsOffCnfm_B_Rq": 0,                     # 0=Inactive, 1=Active [0|1]
-    "LatCtlPath_No_Cnt": counter,               # [0|15]
-    "LatCtlPath_No_Cs": 0,                      # [0|255]
-  }
-
-  # calculate checksum
-  dat = packer.make_can_msg("LateralMotionControl2", CANBUS.main, values)[2]
-  values["LatCtlPath_No_Cs"] = calculate_lat_ctl2_checksum(mode, counter, dat)
-
-  return packer.make_can_msg("LateralMotionControl2", CANBUS.main, values)
-
-
-def create_acc_command(packer, long_active: bool, gas: float, accel: float, precharge_brake: bool, decel: bool):
-  """
-  Creates a CAN message for the Ford ACC Command.
-
-  This command can be used to enable ACC, to set the ACC gas/brake/decel values
-  and to disable ACC.
-
-  Frequency is 50Hz.
-  """
-
-  values = {
-    "AccBrkTot_A_Rq": accel,                          # Brake total accel request: [-20|11.9449] m/s^2
-    "Cmbb_B_Enbl": 1 if long_active else 0,           # Enabled: 0=No, 1=Yes
-    "AccPrpl_A_Rq": gas,                              # Acceleration request: [-5|5.23] m/s^2
-    "AccBrkPrchg_B_Rq": 1 if precharge_brake else 0,  # Pre-charge brake request: 0=No, 1=Yes
-    "AccBrkDecel_B_Rq": 1 if decel else 0,            # Deceleration request: 0=Inactive, 1=Active
-  }
-  return packer.make_can_msg("ACCDATA", CANBUS.main, values)
-
-
-def create_lkas_ui_msg(packer, main_on: bool, enabled: bool, steer_alert: bool, hud_control, stock_values: dict):
-  """
-  Creates a CAN message for the Ford IPC IPMA/LKAS status.
-
-  Show the LKAS status with the "driver assist" lines in the IPC.
-
-  Stock functionality is maintained by passing through unmodified signals.
-
-  Frequency is 1Hz.
-  """
-
-  # LaActvStats_D_Dsply
-  #    R  Intvn Warn Supprs Avail No
-  # L
-  # Intvn  24    19    14     9   4
-  # Warn   23    18    13     8   3
-  # Supprs 22    17    12     7   2
-  # Avail  21    16    11     6   1
-  # No     20    15    10     5   0
-  #
-  # TODO: test suppress state
-  if enabled:
-    lines = 0  # NoLeft_NoRight
-    if hud_control.leftLaneDepart:
-      lines += 4
-    elif hud_control.leftLaneVisible:
-      lines += 1
-    if hud_control.rightLaneDepart:
-      lines += 20
-    elif hud_control.rightLaneVisible:
-      lines += 5
-  elif main_on:
-    lines = 0
+def ParkAid_Data(packer, enabled, apply_steer, sappControlState, standstill):
+  # sappState 1 = Off, 2 = On | sappControl 0 = No request, 1 = Request
+  # No angle request at standstill because it causes sporadic steering wheel drift.
+  if sappControlState in [1, 2]:
+    sappState = 2
+    sappControl = 0
+    if enabled and not standstill:
+      sappControl = 1
   else:
-    if hud_control.leftLaneDepart:
-      lines = 3  # WarnLeft_NoRight
-    elif hud_control.rightLaneDepart:
-      lines = 15  # NoLeft_WarnRight
-    else:
-      lines = 30  # LA_Off
-
-  hands_on_wheel_dsply = 1 if steer_alert else 0
+    sappState = 1
+    sappControl = 0
 
   values = {
-    **stock_values,
-    "LaActvStats_D_Dsply": lines,                 # LKAS status (lines) [0|31]
-    "LaHandsOff_D_Dsply": hands_on_wheel_dsply,   # 0=HandsOn, 1=Level1 (w/o chime), 2=Level2 (w/ chime), 3=Suppressed
+    "ApaSys_D_Stat": sappState,
+    "EPASExtAngleStatReq": sappControl,
+    "ExtSteeringAngleReq2": apply_steer,
   }
-  return packer.make_can_msg("IPMA_Data", CANBUS.main, values)
+  return packer.make_can_msg("ParkAid_Data", 2, values)
 
-
-def create_acc_ui_msg(packer, main_on: bool, enabled: bool, hud_control, stock_values: dict):
-  """
-  Creates a CAN message for the Ford IPC adaptive cruise, forward collision warning and traffic jam assist status.
-
-  Stock functionality is maintained by passing through unmodified signals.
-
-  Frequency is 20Hz.
-  """
-
-  # Tja_D_Stat
-  if enabled:
-    if hud_control.leftLaneDepart:
-      status = 3  # ActiveInterventionLeft
-    elif hud_control.rightLaneDepart:
-      status = 4  # ActiveInterventionRight
-    else:
-      status = 2  # Active
-  elif main_on:
-    if hud_control.leftLaneDepart:
-      status = 5  # ActiveWarningLeft
-    elif hud_control.rightLaneDepart:
-      status = 6  # ActiveWarningRight
-    else:
-      status = 1  # Standby
+def EngVehicleSpThrottle2(packer, frame, speed, gearShifter):
+  # If in reverse, send appropriate data
+  if gearShifter == GearShifter.reverse:
+    reverse = 3
+    trailer = 1
   else:
-    status = 0    # Off
+    reverse = 1
+    trailer = 0
+
+  cnt = frame % 16
+  cs = fordchecksum(cnt, speed)
 
   values = {
-    **stock_values,
-    "Tja_D_Stat": status,
+    "VehVTrlrAid_B_Avail": trailer,
+    "VehVActlEng_No_Cs": cs,
+    "VehVActlEng_No_Cnt": cnt,
+    "VehVActlEng_D_Qf": 3,
+    "Veh_V_ActlEng": speed,
+    "GearRvrse_D_Actl": reverse,
   }
-  return packer.make_can_msg("ACCDATA_3", CANBUS.main, values)
+  return packer.make_can_msg("EngVehicleSpThrottle2", 2, values)
 
-
-def create_button_msg(packer, stock_values: dict, cancel=False, resume=False, tja_toggle=False,
-                      bus: int = CANBUS.camera):
-  """
-  Creates a CAN message for the Ford SCCM buttons/switches.
-
-  Includes cruise control buttons, turn lights and more.
-  """
+def BrakeSysFeatures(packer, frame, speed):
+  cnt = frame % 16
+  cs = fordchecksum(cnt, speed)
 
   values = {
-    **stock_values,
-    "CcAslButtnCnclPress": 1 if cancel else 0,      # CC cancel button
-    "CcAsllButtnResPress": 1 if resume else 0,      # CC resume button
-    "TjaButtnOnOffPress": 1 if tja_toggle else 0,   # TJA toggle button
+    "LsmcBrkDecel_D_Stat": 4,
+    "VehVActlBrk_No_Cs": cs,
+    "Veh_V_ActlBrk": speed,
+    "VehVActlBrk_No_Cnt": cnt,
+    "VehVActlBrk_D_Qf": 3,
   }
-  return packer.make_can_msg("Steering_Data_FD1", bus, values)
+  return packer.make_can_msg("BrakeSysFeatures", 2, values)
