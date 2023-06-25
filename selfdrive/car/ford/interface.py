49644d6b5a27c5a646ad73d7993fa1fa436bd5c1
Shea_D <s.shea.duncan@gmail.com>
2023-06-25 11:47:45 -0500

    Adding CarInterfacebase for F150

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -59,6 +59,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 3.99288
       ret.steerRatio = 17.0
       ret.mass = 2275 + STD_CARGO_KG
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.FOCUS_MK4:
       ret.wheelbase = 2.7
