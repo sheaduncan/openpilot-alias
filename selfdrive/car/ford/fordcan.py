8ba9a30a0bffaa7060166e6b3e546474572a5fe4
Shea_D <s.shea.duncan@gmail.com>
2023-12-15 07:57:17 -0600

    adding a precharge signal based on min_gas

--- selfdrive/car/ford/fordcan.py
+++ selfdrive/car/ford/fordcan.py
@@ -128,7 +128,8 @@ def create_acc_msg(packer, CAN: CanBus, long_active: bool, gas: float, accel: fl
   Frequency is 50Hz.
   """
 
-  actuateBrakes = gas == -5 and long_active
+  precharge = gas < CarControllerParams.MIN_GAS and long_active
+  actuateBrakes = gas < -2 and long_active
   values = {
     "AccBrkTot_A_Rq": accel,                          # Brake total accel request: [-20|11.9449] m/s^2
     "Cmbb_B_Enbl": 1 if long_active else 0,           # Enabled: 0=No, 1=Yes
@@ -137,7 +138,7 @@ def create_acc_msg(packer, CAN: CanBus, long_active: bool, gas: float, accel: fl
     "AccResumEnbl_B_Rq": 1 if long_active else 0,
     "AccVeh_V_Trg": v_ego_kph,                        # Target speed: [0|255] km/h
     # Pre-charging and actuating brake are typically only done in BlueCruise when under engine braking gas (MIN_GAS)
-    "AccBrkPrchg_B_Rq": 1 if actuateBrakes else 0,            # Pre-charge brake request: 0=No, 1=Yes
+    "AccBrkPrchg_B_Rq": 1 if precharge else 0,            # Pre-charge brake request: 0=No, 1=Yes
     "AccBrkDecel_B_Rq": 1 if actuateBrakes else 0,            # Deceleration request: 0=Inactive, 1=Active
     "AccStopStat_B_Rq": 1 if stopping else 0,
   }
