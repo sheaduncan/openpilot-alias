041e8a499b38092b80e95f46160ff7655c2a0f79
Shea_D <s.shea.duncan@gmail.com>
2023-12-21 13:46:25 -0600

    Update interface.py

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -64,15 +64,13 @@ class CarInterface(CarInterfaceBase):
       #ret.vEgoStopping = 0.5
       #ret.vEgoStarting = 0.5
       #ret.stoppingControl = True
-      #ret.longitudinalTuning.deadzoneBP = [0.]
-      #ret.longitudinalTuning.deadzoneV = [0.]
       #ret.longitudinalTuning.kf = 1.
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [0.5]
       ret.longitudinalTuning.kiBP = [0.]
       ret.longitudinalTuning.kiV = [0.]
-      ret.longitudinalTuning.deadzoneBP = [0., 9.]
-      ret.longitudinalTuning.deadzoneV = [.0, .2]
+      #ret.longitudinalTuning.deadzoneBP = [0., 9.]
+      #ret.longitudinalTuning.deadzoneV = [.0, .2]
       # TODO estimate car specific lag, use .15s for now
       #ret.longitudinalActuatorDelayLowerBound = 0.15
       #ret.longitudinalActuatorDelayUpperBound = 0.15
